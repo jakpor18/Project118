4ea8becd97a08f6eb9d00b9b3cbfff838b9db2ba
jakpor18 <95719511+jakpor18@users.noreply.github.com>
2023-01-12 19:56:18 +0000

    Add files via upload

---
+++ ProjectC118/Walkers.py
@@ -0,0 +1,40 @@
+import cv2
+
+
+# Create our body classifier
+face_cascade = cv2.CascadeClassifier("haarcascade_fullbody.xml")
+
+
+# Initiate video capture for video file
+cap = cv2.VideoCapture('walking.avi')
+
+# Loop once video is successfully loaded
+while True:
+    
+    # Read first frame
+    ret, frame = cap.read()
+
+    #Convert Each Frame into Grayscale
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # Detect faces in the frame
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+
+    # Draw a rectangle around the faces
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+    # Display the resulting frame
+    cv2.imshow("frame",frame)
+    # Pass frame to our body classifier
+    
+    
+    
+    # Extract bounding boxes for any bodies identified
+    
+
+    if cv2.waitKey(1) == 32: #32 is the Space Key
+        break
+
+cap.release()
+cv2.destroyAllWindows()
